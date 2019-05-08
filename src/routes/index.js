7c2e3e6d30999a36482e4e498b1456ba3d27304c
WanderingBrooks <cs.jasonbrooks@gmail.com>
2019-05-08 17:18:56 -0400

    Adding all paths to bundler route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,5 @@ module.exports = ( app ) => {
   app.use( '/api/album',    require('./album') );
   app.use( '/api/playlist', require('./playlist') );
   app.use( '/',             require('./bundler') );
+  app.use( '/*',            require('./bundler') );
 };
